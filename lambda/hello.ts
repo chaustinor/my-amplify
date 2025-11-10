b58fa0147e1fd21f9b85fc944561cda1d87b7a4e
Chris C <chris.campbell@presencepg.com>
2025-11-09 20:51:41 -0600

    UPdates

--- lambda/hello.ts
+++ lambda/hello.ts
@@ -3,11 +3,26 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 export const handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
+  // Handle preflight OPTIONS request
+  if (event.requestContext.http.method === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+      body: ''
+    };
+  }
+
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
     },
     body: JSON.stringify({
       message: 'Hello World from Lambda! You did it Chris!!',
